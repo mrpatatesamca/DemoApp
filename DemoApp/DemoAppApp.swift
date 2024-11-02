5820db67afee6518cd4c79d9bb289df90dd9c8c5
mrpatatesamca <berkeaydin618@gmail.com>
2024-11-02 11:05:42 +0300

    Initial Commit

---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by Berke Aydin on 2.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DemoAppTests/DemoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppTests.swift
+//  DemoAppTests
+//
+//  Created by Berke Aydin on 2.11.2024.
+//
+
+import Testing
+@testable import DemoApp
+
+struct DemoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
